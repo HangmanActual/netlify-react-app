e392bf666214893e3d8869f53441b51277c1b663
HangmanActual <jasonwei314@gmail.com>
2023-06-08 16:28:41 -0700

    removed index.html from src; edited App.js <p> message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Successfully Deployed <code>A React application</code> to Netlify with CircleCI
+          Successfully Deployed <code>A React application</code> to Netlify withOUT CircleCI
         </p>
         <a
           className="App-link"
